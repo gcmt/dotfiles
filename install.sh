4c6257563ba8c8859108d182e7f3f803c4e4bf55
Giacomo Comitti <dev@gcomit.com>
2018-02-20 06:14:58 +0100

    Refactor installation script

--- install.sh
+++ install.sh
@@ -4,212 +4,99 @@ DOTDIR="$(cd "$(dirname "$0")" && pwd)"
 
 source "$DOTDIR/zsh/.zshenv"
 
-if [ -z "$XDG_CONFIG_HOME" -o -z "$XDG_CACHE_HOME" -o -z "$XDG_DATA_HOME" ]
-then
+if [ -z "$XDG_CONFIG_HOME" -o -z "$XDG_CACHE_HOME" -o -z "$XDG_DATA_HOME" ]; then
 	echo >&2 'XDG directories not set'
 	exit 1
 fi
+
+link() {
+	if [[ -d "$1" ]]; then
+		ln -sfn "$1" "$2"
+	else
+		ln -sf "$1" "$2"
+	fi
+	echo "$2 -> $1"
+}
 
 mkdir -p "$XDG_DATA_HOME"
 mkdir -p "$XDG_CACHE_HOME"
 mkdir -p "$XDG_CONFIG_HOME"
 
 if [[ "$@" =~ '-x' || "$@" =~ '-all' ]]; then
-	if hash xinit 2>/dev/null
-	then
-		ln -sf "$DOTDIR/X11/xinitrc" "$HOME/.xinitrc"
-		ln -sf "$DOTDIR/X11/Xmodmap" "$HOME/.Xmodmap"
-		echo "[ ok ] x"
-	else
-		echo "[ skip ] x"
-	fi
+	link "$DOTDIR/X11/xinitrc" "$HOME/.xinitrc"
+	link "$DOTDIR/X11/Xmodmap" "$HOME/.Xmodmap"
 fi
 
-if [[ "$@" =~ '-fontconfig' || "$@" =~ '-all' ]]
-then
-	if hash fc-list 2>/dev/null
-	then
-		ln -snf "$DOTDIR/fontconfig" "$XDG_CONFIG_HOME/fontconfig"
-		echo "[ ok ] fontconfig"
-	else
-		echo "[ skip ] fontconfig"
-	fi
+if [[ "$@" =~ '-fontconfig' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/fontconfig" "$XDG_CONFIG_HOME/fontconfig"
 fi
 
-if [[ "$@" =~ '-rofi' || "$@" =~ '-all' ]]
-then
-	if hash rofi 2>/dev/null
-	then
-		ln -snf "$DOTDIR/rofi" "$XDG_CONFIG_HOME/rofi"
-		echo "[ ok ] rofi"
-	else
-		echo "[ skip ] rofi"
-	fi
+if [[ "$@" =~ '-rofi' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/rofi" "$XDG_CONFIG_HOME/rofi"
 fi
 
 if [[ "$@" =~ '-compton' || "$@" =~ '-all' ]]; then
-	if hash compton 2>/dev/null
-	then
-		ln -sf "$DOTDIR/compton/compton.conf" "$XDG_CONFIG_HOME/compton.conf"
-		echo "[ ok ] compton"
-	else
-		echo "[ skip ] compton"
-	fi
+	link "$DOTDIR/compton/compton.conf" "$XDG_CONFIG_HOME/compton.conf"
 fi
 
-if [[ "$@" =~ '-i3' || "$@" =~ '-all' ]]
-then
-	if hash i3 2>/dev/null
-	then
-		mkdir -p "$XDG_DATA_HOME/i3"
-		ln -snf "$DOTDIR/i3" "$XDG_CONFIG_HOME/i3"
-		echo "[ ok ] i3"
-	else
-		echo "[ skip ] i3"
-	fi
+if [[ "$@" =~ '-i3' || "$@" =~ '-all' ]]; then
+	mkdir -p "$XDG_DATA_HOME/i3"
+	link "$DOTDIR/i3" "$XDG_CONFIG_HOME/i3"
 fi
 
-if [[ "$@" =~ '-polybar' || "$@" =~ '-all' ]]
-then
-	if hash polybar 2>/dev/null
-	then
-		ln -snf "$DOTDIR/polybar" "$XDG_CONFIG_HOME/polybar"
-		echo "[ ok ] polybar"
-	else
-		echo "[ skip ] polybar"
-	fi
+if [[ "$@" =~ '-polybar' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/polybar" "$XDG_CONFIG_HOME/polybar"
 fi
-if [[ "$@" =~ '-rofi' || "$@" =~ '-all' ]]
-then
-	if hash rofi 2>/dev/null
-	then
-		ln -snf "$DOTDIR/rofi" "$XDG_CONFIG_HOME/rofi"
-		echo "[ ok ] rofi"
-	else
-		echo "[ skip ] rofi"
-	fi
+if [[ "$@" =~ '-rofi' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/rofi" "$XDG_CONFIG_HOME/rofi"
 fi
 
-if [[ "$@" =~ '-termite' || "$@" =~ '-all' ]]
-then
-	if hash termite 2>/dev/null
-	then
-		ln -snf "$DOTDIR/termite" "$XDG_CONFIG_HOME/termite"
-		echo "[ ok ] termite"
-	else
-		echo "[ skip ] termite"
-	fi
+if [[ "$@" =~ '-termite' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/termite" "$XDG_CONFIG_HOME/termite"
 fi
 
-if [[ "$@" =~ '-vim' || "$@" =~ '-all' ]]
-then
-	if hash vim 2>/dev/null
-	then
-		mkdir -p "$XDG_DATA_HOME/vim"
-		mkdir -p "$XDG_CACHE_HOME/vim/undofiles"
-		ln -snf "$DOTDIR/vim" "$XDG_CONFIG_HOME/vim"
-		echo "[ ok ] vim"
-	else
-		echo "[ skip ] vim"
-	fi
+if [[ "$@" =~ '-vim' || "$@" =~ '-all' ]]; then
+	mkdir -p "$XDG_DATA_HOME/vim"
+	mkdir -p "$XDG_CACHE_HOME/vim/undofiles"
+	link "$DOTDIR/vim" "$XDG_CONFIG_HOME/vim"
 fi
 
-if [[ "$@" =~ '-zsh' || "$@" =~ '-all' ]]
-then
-	if hash zsh 2>/dev/null
-	then
-		mkdir -p "$XDG_DATA_HOME/zsh"
-		mkdir -p "$XDG_DATA_HOME/zsh/ext"
-		ln -sf "$DOTDIR/zsh/.zshenv" "$HOME/.zshenv"
-		ln -sf "$DOTDIR/zsh/rofi.zsh" "$XDG_DATA_HOME/zsh/ext/rofi.zsh"
-		ln -snf "$DOTDIR/zsh" "$XDG_CONFIG_HOME/zsh"
-		echo "[ ok ] zsh"
-	else
-		echo "[ skip ] zsh"
-	fi
+if [[ "$@" =~ '-zsh' || "$@" =~ '-all' ]]; then
+	mkdir -p "$XDG_DATA_HOME/zsh"
+	mkdir -p "$XDG_DATA_HOME/zsh/ext"
+	link "$DOTDIR/zsh" "$XDG_CONFIG_HOME/zsh"
+	link "$DOTDIR/zsh/.zshenv" "$HOME/.zshenv"
+	link "$DOTDIR/zsh/rofi.zsh" "$XDG_DATA_HOME/zsh/ext/rofi.zsh"
 fi
 
-if [[ "$@" =~ '-tmux' || "$@" =~ '-all' ]]
-then
-	if hash tmux 2>/dev/null
-	then
-		ln -sf "$DOTDIR/tmux/tmux.conf" "$HOME/.tmux.conf"
-		echo "[ ok ] tmux"
-	else
-		echo "[ skip ] tmux"
-	fi
+if [[ "$@" =~ '-tmux' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/tmux/tmux.conf" "$HOME/.tmux.conf"
 fi
 
-if [[ "$@" =~ '-git' || "$@" =~ '-all' ]]
-then
-	if hash git 2>/dev/null
-	then
-		ln -snf "$DOTDIR/git" "$XDG_CONFIG_HOME/git"
-		echo "[ ok ] git"
-	else
-		echo "[ skip ] git"
-	fi
+if [[ "$@" =~ '-git' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/git" "$XDG_CONFIG_HOME/git"
 fi
 
-if [[ "$@" =~ '-ctags' || "$@" =~ '-all' ]]
-then
-	if hash ctags 2>/dev/null
-	then
-		ln -sf "$DOTDIR/ctags/ctags" "$HOME/.ctags"
-		echo "[ ok ] ctags"
-	else
-		echo "[ skip ] ctags"
-	fi
+if [[ "$@" =~ '-ctags' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/ctags/ctags" "$HOME/.ctags"
 fi
 
-if [[ "$@" =~ '-elixir' || "$@" =~ '-all' ]]
-then
-	if hash iex 2>/dev/null
-	then
-		ln -sf "$DOTDIR/elixir/iex.exs" "$HOME/.iex.exs"
-		echo "[ ok ] iex.exs"
-	else
-		echo "[ skip ] iex.exs"
-	fi
+if [[ "$@" =~ '-elixir' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/elixir/iex.exs" "$HOME/.iex.exs"
 fi
 
-if [[ "$@" =~ '-gtk' || "$@" =~ '-all' ]]
-then
-	if hash gtk3-demo 2>/dev/null
-	then
-		ln -snf "$DOTDIR/gtk-3.0" "$XDG_CONFIG_HOME/gtk-3.0"
-		echo "[ ok ] gtk3"
-	else
-		echo "[ skip ] gtk3"
-	fi
+if [[ "$@" =~ '-gtk' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/gtk-3.0" "$XDG_CONFIG_HOME/gtk-3.0"
 fi
 
-if [[ "$@" =~ '-dunst' || "$@" =~ '-all' ]]
-then
-	if hash dunst 2>/dev/null
-	then
-		ln -snf "$DOTDIR/dunst" "$XDG_CONFIG_HOME/dunst"
-		echo "[ ok ] dunst"
-	else
-		echo "[ skip ] dunst"
-	fi
+if [[ "$@" =~ '-dunst' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/dunst" "$XDG_CONFIG_HOME/dunst"
 fi
 
-if [[ "$@" =~ '-zathura' || "$@" =~ '-all' ]]
-then
-	if hash zathura 2>/dev/null
-	then
-		ln -snf "$DOTDIR/zathura" "$XDG_CONFIG_HOME/zathura"
-		echo "[ ok ] zathura"
-	else
-		echo "[ skip ] zathura"
-	fi
+if [[ "$@" =~ '-zathura' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/zathura" "$XDG_CONFIG_HOME/zathura"
 fi
 
-if [[ "$@" =~ '-mailcheck' || "$@" =~ '-all' ]]
-then
-	ln -snf "$DOTDIR/mailcheck" "$XDG_DATA_HOME/mailcheck"
-	echo "[ ok ] mailcheck"
+if [[ "$@" =~ '-mailcheck' || "$@" =~ '-all' ]]; then
+	link "$DOTDIR/mailcheck" "$XDG_DATA_HOME/mailcheck"
 fi
-
-unset DOTDIR

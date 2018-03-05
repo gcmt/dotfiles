6c3b212228ee4c7ba3ff56b36ea76e976012817c
Giacomo Comitti <dev@gcomit.com>
2018-03-05 05:13:24 +0100

    Update battery module

--- polybar/modules/battery.sh
+++ polybar/modules/battery.sh
@@ -1,43 +1,40 @@
 #!/bin/bash
 
-last_notification=0
+last_notification="/tmp/polybar-battery"
+
+if [[ ! -f "$last_notification" ]]; then
+	echo 0 > "$last_notification"
+fi
 
 is_charging() {
 	acpi -b | grep -q Charging
 }
 
-# usage: notify N "Message"
+# Usage: notify N message
 # Don't send notification if at least N seconds haven't passed since the last notification
 notify() {
 	is_charging && return
-	local elapsed=$(expr $(date '+%s') - $last_notification)
+	local elapsed=$(($(date +'%s') - $(cat "$last_notification")))
 	if (( $elapsed > $1 )); then
-		last_notification=$(date '+%s')
+		date +'%s' > "$last_notification"
 		notify-send -t 10000 "Battery low" "$2"
 	fi
 }
 
-while true; do
+charge=$(acpi -b | grep -Po '[0-9]+(?=%)')
 
-	charge=$(acpi -b | grep -Po '[0-9]+(?=%)')
+if (( $charge < 15 )); then
+	notify 120 "Remaining charge:  $charge%"
+	icon=""
+elif (( $charge < 30 )); then
+	notify 240 "Remaining charge:  $charge%"
+	icon=""
+elif (( $charge < 50 )); then
+	icon=""
+elif (( $charge < 75 )); then
+	icon=""
+else
+	icon=""
+fi
 
-	if (( $charge < 15 )); then
-		notify 120 "Remaining charge:  $charge%"
-		icon=""
-	elif (( $charge < 30 )); then
-		notify 360 "Remaining charge:  $charge%"
-		icon=""
-	elif (( $charge < 50 )); then
-		icon=""
-	elif (( $charge < 75 )); then
-		icon=""
-	else
-		icon=""
-	fi
-
-	echo "$icon $charge%"
-
-	sleep 60 &
-	wait
-
-done
+echo "$icon $charge%"

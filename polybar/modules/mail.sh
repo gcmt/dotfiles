e948dec967b0a46231143cd4860f5fcc9a83a760
Giacomo Comitti <dev@gcomit.com>
2018-03-17 16:12:52 +0100

    Improve email notifications

--- polybar/modules/mail.sh
+++ polybar/modules/mail.sh
@@ -1,51 +1,55 @@
 #!/bin/bash
 
+dir=/run/mailcheck
+count=0
+count_prev=0
+hidden=0
+pid=
+
+if [[ ! -d "$dir" ]]; then
+	exit 1
+fi
+
 hide_notification() {
 	hidden=1
 	echo
 }
+trap 'hide_notification' SIGUSR2
 
 view_mail() {
 	firefox -new-tab 'https://www.fastmail.com/mail/'
 	i3-msg '[class="Firefox"] focus' >/dev/null 2>&1
 	echo
 }
+trap 'view_mail' SIGUSR1
 
-check_mail() {
-	local count
-	count=$(mail-check.py 2>/dev/null)
-	if [[ $? -eq 0 && "$count" -gt 0 ]]; then
-		if [[ $hidden -eq 0 || $hidden -eq 1 && "$count" -ne "$count_prev" ]]; then
-			count_prev=$count
-			hidden=0
-			echo "$count"
+notify() {
+	count=0
+	for mailbox in "$dir"/*; do
+		if [[ ! "$mailbox" =~ Trash|Spam|Sent|Queue|Drafts|Archive|Notes|LinkedIn|News ]]; then
+			count=$(expr $count + $(cat $mailbox))
 		fi
+	done
+	if [[ $count == 0 ]]; then
+		echo
+	elif [[ $hidden -eq 0 || $hidden -eq 1 && $count -ne $count_prev ]]; then
+		hidden=0
+		count_prev=$count
+		echo "$count"
 	fi
 }
 
-trap "view_mail" SIGUSR1
-trap "hide_notification" SIGUSR2
+watch_for_updates() {
+	inotifywait -qm -e close_write "$dir" |
+		while read path action file; do
+			notify
+		done
+}
 
-source ~/.fastmail
-
-interval=60
-count_prev=0
-hidden=0
-sleep_pid=
+notify
 
 while true; do
-
-	[[ $sleep_pid ]] && kill $sleep_pid 2>/dev/null
-	sleep $interval &
-	sleep_pid=$!
-
+	watch_for_updates & pid=$!
 	wait 2>/dev/null
-	if (( $? > 127 )); then
-		# When SIGUSR1/SIGUSR2 is received, wait immediately returns
-		# (with 138/140 exit codes)
-		continue
-	fi
-
-	check_mail
-
+	kill $pid 2>/dev/null
 done

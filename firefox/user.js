4b14a8d003ace93141cb83ce0fcfca2b286cc16b
Giacomo Comitti <dev@gcomit.com>
2018-04-09 10:12:48 +0200

    Add firefox settings

---
+++ firefox/user.js
@@ -0,0 +1,18 @@
+
+user_pref("general.warnOnAboutConfig", false);
+
+user_pref("permissions.default.desktop-notification", 2);
+
+user_pref("privacy.trackingprotection.enabled", true);
+
+user_pref("extensions.pocket.enabled", false);
+
+user_pref("browser.bookmarks.showMobileBookmarks", true);
+
+user_pref("browser.search.region", "US");
+
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.tabs.warnOnClose", false);
+
+user_pref("browser.urlbar.doubleClickSelectsAll", false);
+user_pref("browser.urlbar.oneOffSearches", false);

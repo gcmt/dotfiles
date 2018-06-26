f3c544a37653fd295ebdb1bfa6169432b08c376a
Giacomo Comitti <dev@gcomit.com>
2018-06-26 15:31:25 +0200

    Fix up dotfiles installation script

--- install.sh
+++ install.sh
@@ -114,10 +114,6 @@ if [[ "$@" =~ '-zathura' || "$@" =~ '-all' ]]; then
 	link "$DOTDIR/zathura" "$XDG_CONFIG_HOME/zathura"
 fi
 
-if [[ "$@" =~ '-mailcheck' || "$@" =~ '-all' ]]; then
-	link "$DOTDIR/mailcheck" "$XDG_DATA_HOME/mailcheck"
-fi
-
 if [[ "$@" =~ '-cmus' || "$@" =~ '-all' ]]; then
 	mkdir -p "$XDG_CONFIG_HOME/cmus"
 	link "$DOTDIR/cmus/rc" "$XDG_CONFIG_HOME/cmus/rc"

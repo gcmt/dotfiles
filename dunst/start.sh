64a5856b88c6392d404ec9740ab2a62436c4100d
Giacomo Comitti <dev@gcomit.com>
2018-08-02 15:35:37 +0200

    Fixup dunst startup script

--- dunst/start.sh
+++ dunst/start.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 pkill -x dunst
 while pgrep -x dunst >/dev/null; do sleep 1; done
 
 dunst &
 
-if [[ "$1" == "-test" ]];
-then
+if [[ "$1" == "-test" ]]; then
 	notify-send -u critical "Title" "Critical urgency message"
 	notify-send -u normal "Title" "Normal urgency message"
 	notify-send -u low "Title" "Low urgency message"

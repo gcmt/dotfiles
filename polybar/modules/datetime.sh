dfc8a8b5bf0ba80da53402a68247290104bdacce
Giacomo Comitti <dev@gcomit.com>
2018-08-12 14:31:41 +0200

    Use rofi menu for choosing datetime style

--- polybar/modules/datetime.sh
+++ polybar/modules/datetime.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 
-utc=0
+state_file="$HOME/.local/share/polybar/datetime"
 
-toggle() {
-	utc=$((1 - $utc))
-}
+if [[ ! -e "$state_file" ]]; then
+	mkdir -p "$(dirname "$state_file")"
+	echo "style=default" > "$state_file"
+fi
 
-trap "toggle" SIGUSR1
+style=$(grep -Po '(?<=style=).*' "$state_file" 2>/dev/null)
+style=${style:-default}
 
-while true; do
+if [[ "$*" =~ -menu($| ) ]]; then
 
-	if (( $utc == 1 )); then
-		echo "$(date --utc '+%a %d %H:%M') UTC"
-	 else
-		date '+%a %d %H:%M'
+	styles=(default seconds utc)
+	entries="default\\nshow seconds\\nutc time"
+
+	rofi_options="-dmenu -format i -click-to-exit -select '$style'"
+	rofi_style="-theme 'polybar.rasi' -theme-str 'window { width:15%; location:north; anchor:north; }'"
+
+	idx=$(echo -e "$entries" | eval "rofi $rofi_options $rofi_style")
+	if [[ -z "$idx" ]]; then
+		exit 1
 	fi
 
-	sleep 1 &
-	wait
+	style="${styles[$idx]}"
+	sed -i "s/^style=.*/style=$style/" "$state_file"
 
-done
+fi
+
+case "$style" in
+	utc)
+		echo "$(date --utc '+%a %d %H:%M') UTC"
+		;;
+	seconds)
+		date '+%a %d %H:%M:%S'
+		;;
+	*)
+		date '+%a %d %H:%M'
+		;;
+esac

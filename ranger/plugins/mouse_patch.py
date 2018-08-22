54fa52de8b311bfad7526291eba2311cf94e7b5e
Giacomo Comitti <dev@gcomit.com>
2018-08-22 18:32:16 +0200

    Change mouse click actions

---
+++ ranger/plugins/mouse_patch.py
@@ -0,0 +1,57 @@
+
+import ranger
+from ranger.gui.widgets.browsercolumn import BrowserColumn
+
+
+HOOK_INIT_OLD = ranger.api.hook_init
+
+
+def click(self, event):
+
+    direction = event.mouse_wheel_direction()
+    if not (event.pressed(1) or event.pressed(3) or direction):
+        return False
+
+    if self.target is None or not self.target.is_directory:
+        return False
+
+    if not self.target.accessible and not self.target.content_loaded:
+        return False
+
+    index = self.scroll_begin + event.y - self.y
+
+    if direction:
+
+        if self.level == -1:
+            self.fm.move_parent(direction)
+        else:
+            return False
+
+    # right click
+    elif event.pressed(3):
+
+        self.fm.move(left=1)
+
+    # left click
+    elif event.pressed(1):
+
+        try:
+            clicked_file = self.target.files[index]
+        except IndexError:
+            return False
+
+        if clicked_file.is_directory:
+            self.fm.enter_dir(clicked_file.path, remember=True)
+        elif self.level == 0:
+            self.fm.thisdir.move_to_obj(clicked_file)
+            self.fm.execute_file(clicked_file)
+
+    return True
+
+
+def hook_init(fm):
+    BrowserColumn.click = click
+    return HOOK_INIT_OLD(fm)
+
+
+ranger.api.hook_init = hook_init

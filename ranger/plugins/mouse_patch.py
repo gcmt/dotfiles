5d91fb9632e76aad9d3884e70d2ec485fbd0c182
Giacomo Comitti <dev@gcomit.com>
2018-08-22 21:46:14 +0200

    Double click to open a file

--- ranger/plugins/mouse_patch.py
+++ ranger/plugins/mouse_patch.py
@@ -1,16 +1,27 @@
 
 import os
+from datetime import datetime
+
 import ranger
 from ranger.gui.widgets.browsercolumn import BrowserColumn
 
 
 HOOK_INIT_OLD = ranger.api.hook_init
 
+DOUBLE_CLICK_TRESHOLD = 500
+
+LEFT_BTN = 1
+RIGHT_BTN = 3
+
+LAST_CLICK = {
+    LEFT_BTN: datetime.now()
+}
+
 
 def click(self, event):
 
     direction = event.mouse_wheel_direction()
-    if not (event.pressed(1) or event.pressed(3) or direction):
+    if not (event.pressed(LEFT_BTN) or event.pressed(RIGHT_BTN) or direction):
         return False
 
     if self.target is None or not self.target.is_directory:
@@ -21,7 +32,6 @@ def click(self, event):
 
     index = self.scroll_begin + event.y - self.y
 
-    # Mouse scroll
     if direction:
 
         if self.level == -1:
@@ -29,15 +39,13 @@ def click(self, event):
         else:
             return False
 
-    # Right click
     # Go to parent directory
-    elif event.pressed(3):
+    elif event.pressed(RIGHT_BTN):
 
         self.fm.move(left=1)
 
-    # Left click
-    # Enter directory or execute file
-    elif event.pressed(1):
+    # Enter directory or select file. Double click on a file opens it.
+    elif event.pressed(LEFT_BTN):
 
         try:
             clicked_file = self.target.files[index]
@@ -45,10 +53,20 @@ def click(self, event):
             return False
 
         if clicked_file.is_directory:
+
             self.fm.enter_dir(clicked_file.path, remember=True)
+
         elif self.level == 0:
-            self.fm.thisdir.move_to_obj(clicked_file)
-            self.fm.execute_file(clicked_file)
+
+            delta = datetime.now() - LAST_CLICK[LEFT_BTN]
+            millis = (delta.days * 86400000) + (delta.seconds * 1000) + (delta.microseconds / 1000)
+
+            if clicked_file == self.fm.thisfile and millis < DOUBLE_CLICK_TRESHOLD:
+                self.fm.execute_file(clicked_file)
+            else:
+                self.fm.thisdir.move_to_obj(clicked_file)
+
+        LAST_CLICK[LEFT_BTN] = datetime.now()
 
     return True
 

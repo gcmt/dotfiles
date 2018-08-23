efa1f530e9e2de0940efea546b224ca8bc0c15b7
Giacomo Comitti <dev@gcomit.com>
2018-08-23 14:35:59 +0200

    Add ability to use single or double click to enter directories

--- ranger/plugins/mouse_patch.py
+++ ranger/plugins/mouse_patch.py
@@ -7,9 +7,12 @@ import ranger
 from ranger.gui.widgets.browsercolumn import BrowserColumn
 from ranger.gui.mouse_event import MouseEvent
 
+from .menu import Menu
+
 
 HOOK_INIT_OLD = ranger.api.hook_init
 
+ENTER_DIRS_WITH_SINGLE_CLICK = False
 DOUBLE_CLICK_TRESHOLD = 500
 
 LEFT_BTN = 1
@@ -43,6 +46,11 @@ def click(self, event):
 
     index = self.scroll_begin + event.y - self.y
 
+    try:
+        clicked_file = self.target.files[index]
+    except IndexError:
+        return False
+
     if direction:
 
         if self.level == -1:
@@ -58,21 +66,23 @@ def click(self, event):
     # Enter directory or select file. Double click on a file opens it.
     elif event.pressed(LEFT_BTN):
 
-        try:
-            clicked_file = self.target.files[index]
-        except IndexError:
-            return False
+        delta = datetime.now() - LAST_CLICK[LEFT_BTN]
+        millis = (delta.days * 86400000) + (delta.seconds * 1000) + (delta.microseconds / 1000)
 
         if clicked_file.is_directory:
 
-            self.fm.enter_dir(clicked_file.path, remember=True)
+            if ENTER_DIRS_WITH_SINGLE_CLICK:
+                self.fm.enter_dir(clicked_file.path, remember=True)
+            else:
+                if clicked_file == self.fm.thisfile and millis < DOUBLE_CLICK_TRESHOLD:
+                    self.fm.enter_dir(clicked_file.path, remember=True)
+                else:
+                    self.fm.thisdir.move_to_obj(clicked_file)
 
         elif self.level == 0:
 
-            delta = datetime.now() - LAST_CLICK[LEFT_BTN]
-            millis = (delta.days * 86400000) + (delta.seconds * 1000) + (delta.microseconds / 1000)
-
-            if clicked_file == self.fm.thisfile and millis < DOUBLE_CLICK_TRESHOLD:
+            if clicked_file == self.fm.thisfile \
+                    and millis < DOUBLE_CLICK_TRESHOLD:
                 self.fm.execute_file(clicked_file)
             else:
                 self.fm.thisdir.move_to_obj(clicked_file)

0495487bd2324b1860c95dad3481f96e1320b61d
Giacomo Comitti <dev@gcomit.com>
2018-10-06 15:23:47 +0200

    Join arguments directly with "$*"

--- nemo/actions/copy-path-to-clipboard.sh
+++ nemo/actions/copy-path-to-clipboard.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-paths=()
-while (( $# )); do
-	paths+=("$1")
-	shift
-done
-
 IFS=$'\n'
-echo -n "${paths[*]}" | xclip -sel clipboard
+echo -n "$*" | xclip -sel clipboard
 
-(( ${#paths[*]} > 1 )) && s=s || s=
-notify-send 'Path Copied' "${#paths[*]} path$s copied to the system clipboard"
+(( $# > 1 )) && s=s || s=
+notify-send "Nemo" "$# path$s copied to the system clipboard"

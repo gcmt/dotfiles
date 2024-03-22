bda079b81b4f78c74613f37a95f23f1f7969dfb2
Giacomo Comitti <dev@gcomit.com>
2024-03-22 10:18:54 +0100

    Add borders to hover/signature popups

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -130,6 +130,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = "single",
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	border = "single",
+})
+
 -- TREESITTER
 ----------------------------------------------------------------------------
 
